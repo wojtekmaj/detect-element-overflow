9cb21187975f21976a0915d5616917b46160199c
Wojciech Maj <kontakt@wojtekmaj.pl>
2017-09-05 21:51:57 +0200

    Initial commit

---
+++ build/entry.js
@@ -0,0 +1,35 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+var detectCollisions = function detectCollisions(element, container) {
+  return {
+    get collidedTop() {
+      return element.getBoundingClientRect().top < container.getBoundingClientRect().top;
+    },
+    get collidedBottom() {
+      return element.getBoundingClientRect().bottom > container.getBoundingClientRect().bottom;
+    },
+    get collidedLeft() {
+      return element.getBoundingClientRect().left < container.getBoundingClientRect().left;
+    },
+    get collidedRight() {
+      return element.getBoundingClientRect().right > container.getBoundingClientRect().right;
+    },
+    get overflowTop() {
+      return container.getBoundingClientRect().top - element.getBoundingClientRect().top;
+    },
+    get overflowBottom() {
+      return element.getBoundingClientRect().bottom - container.getBoundingClientRect().bottom;
+    },
+    get overflowLeft() {
+      return container.getBoundingClientRect().left - element.getBoundingClientRect().left;
+    },
+    get overflowRight() {
+      return element.getBoundingClientRect().right - container.getBoundingClientRect().right;
+    }
+  };
+};
+
+exports.default = detectCollisions;
---
+++ src/entry.js
@@ -0,0 +1,28 @@
+const detectCollisions = (element, container) => ({
+  get collidedTop() {
+    return element.getBoundingClientRect().top < container.getBoundingClientRect().top;
+  },
+  get collidedBottom() {
+    return element.getBoundingClientRect().bottom > container.getBoundingClientRect().bottom;
+  },
+  get collidedLeft() {
+    return element.getBoundingClientRect().left < container.getBoundingClientRect().left;
+  },
+  get collidedRight() {
+    return element.getBoundingClientRect().right > container.getBoundingClientRect().right;
+  },
+  get overflowTop() {
+    return container.getBoundingClientRect().top - element.getBoundingClientRect().top;
+  },
+  get overflowBottom() {
+    return element.getBoundingClientRect().bottom - container.getBoundingClientRect().bottom;
+  },
+  get overflowLeft() {
+    return container.getBoundingClientRect().left - element.getBoundingClientRect().left;
+  },
+  get overflowRight() {
+    return element.getBoundingClientRect().right - container.getBoundingClientRect().right;
+  },
+});
+
+export default detectCollisions;
