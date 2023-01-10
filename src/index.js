b13298d4ee0a5a09f80be73cfd62ac785e5680d7
Wojciech Maj <kontakt@wojtekmaj.pl>
2023-01-10 11:17:09 +0100

    Change arrow functions to named functions

--- src/index.js
+++ src/index.js
@@ -1,30 +1,32 @@
-const getRect = (element) => element.getBoundingClientRect();
+function getRect(element) {
+  return element.getBoundingClientRect();
+}
 
-const detectElementOverflow = (element, container) => ({
-  get collidedTop() {
-    return getRect(element).top < getRect(container).top;
-  },
-  get collidedBottom() {
-    return getRect(element).bottom > getRect(container).bottom;
-  },
-  get collidedLeft() {
-    return getRect(element).left < getRect(container).left;
-  },
-  get collidedRight() {
-    return getRect(element).right > getRect(container).right;
-  },
-  get overflowTop() {
-    return getRect(container).top - getRect(element).top;
-  },
-  get overflowBottom() {
-    return getRect(element).bottom - getRect(container).bottom;
-  },
-  get overflowLeft() {
-    return getRect(container).left - getRect(element).left;
-  },
-  get overflowRight() {
-    return getRect(element).right - getRect(container).right;
-  },
-});
-
-export default detectElementOverflow;
+export default function detectElementOverflow(element, container) {
+  return {
+    get collidedTop() {
+      return getRect(element).top < getRect(container).top;
+    },
+    get collidedBottom() {
+      return getRect(element).bottom > getRect(container).bottom;
+    },
+    get collidedLeft() {
+      return getRect(element).left < getRect(container).left;
+    },
+    get collidedRight() {
+      return getRect(element).right > getRect(container).right;
+    },
+    get overflowTop() {
+      return getRect(container).top - getRect(element).top;
+    },
+    get overflowBottom() {
+      return getRect(element).bottom - getRect(container).bottom;
+    },
+    get overflowLeft() {
+      return getRect(container).left - getRect(element).left;
+    },
+    get overflowRight() {
+      return getRect(element).right - getRect(container).right;
+    },
+  };
+}
